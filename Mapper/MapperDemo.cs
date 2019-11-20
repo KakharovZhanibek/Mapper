3ca7a955b3b1e93a55dc12c22811083d61002543
z.kontra <z.kontra@mail.ru>
2019-11-20 16:23:11 +0600

    Добавьте файлы проекта.

---
+++ Mapper/MapperDemo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mapper_
+{
+    public static class MapperExtension
+    {
+        public static TDestination Map<TDestination>(this object source)
+        {
+            return default(TDestination);
+        }
+    }
+
+    public class Mapper<TSource, TDestination>
+    {
+        //public TDestination Map(TSource source)
+        //{
+        //    return default(TDestination);
+        //}
+
+        public TDestination Map(TSource userVm)
+        {
+            TDestination destination = Activator.CreateInstance<TDestination>();
+            Type newUserEdited = destination.GetType();
+            var newUserProperties = newUserEdited.GetProperties();
+
+            Type userVmType = userVm.GetType();
+
+            var userVmProperties = userVmType.GetProperties();
+
+            foreach (var UserProperty in newUserProperties)
+            {
+                foreach (var UserVmProperty in userVmProperties)
+                {
+                    if (UserProperty.PropertyType == UserVmProperty.PropertyType
+                        && UserProperty.Name == UserVmProperty.Name)
+                    {
+                        var VmPropertyValue = UserVmProperty.GetValue(userVm);
+                        UserProperty.SetValue(destination, VmPropertyValue);
+                    }
+                }
+            }
+
+            return destination;
+        }
+    }
+}
---
+++ Mapper/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mapper_
+{
+    internal class PaymentEntity
+    {
+        public Guid Id { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public string PaymentCardNumber { get; set; }
+        public decimal PaymentAmount { get; set; }
+    }
+
+
+    internal class PaymentDetailModel
+    {
+        public DateTime PaymentDate { get; set; }
+        public string PaymentCardNumber { get; set; }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var source = new PaymentEntity()
+            {
+                Id = Guid.NewGuid(),
+                PaymentDate = new DateTime(2017, 12, 11),
+                PaymentCardNumber = "0000-0000-0000-0001",
+                PaymentAmount = 500
+            };
+
+            #region API V1
+
+            var destinationV1 = source.Map<PaymentDetailModel>();
+            #endregion
+
+            #region API V2
+
+            var mapper = new Mapper<PaymentEntity, PaymentDetailModel>();
+            var destinationV2 = mapper.Map(source);
+            #endregion
+        }
+    }
+}
